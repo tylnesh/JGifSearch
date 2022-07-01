b6e822e1f4fd88a61124f265c9cbc87d277e2218
tylnesh <kohutek.michal@gmail.com>
2022-07-01 18:07:24 +0200

    Added jfoenix and javax.annotation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.lordsoftech {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.json;
+    requires com.jfoenix;
+    requires javax.annotation;
 
     opens com.lordsoftech to javafx.fxml;
     exports com.lordsoftech;
