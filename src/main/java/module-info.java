e15cf30f5416d78f3395d29bc0bc7831f9faf8e4
tylnesh <kohutek.michal@gmail.com>
2022-07-04 15:41:27 +0200

    Don't need javax.annotation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.lordsoftech {
     requires javafx.fxml;
     requires org.json;
     requires com.jfoenix;
-    requires javax.annotation;
 
     opens com.lordsoftech to javafx.fxml;
     exports com.lordsoftech;
